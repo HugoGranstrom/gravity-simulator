37ca6ce276d3592b2af86894d8a66ce89916eaea
Hugo <hajenzoo29@gmail.com>
2018-12-16 11:23:06 +0100

    modularize headless

---
+++ headless_init.py
@@ -0,0 +1,177 @@
+### General imports
+import json
+import platform
+from itertools import combinations
+import integrators_headless as integrators
+
+### containerVector ###
+from collections import namedtuple
+conVec = namedtuple("conVec","x y")
+
+### Vector Class ###
+try:
+    if platform.python_implementation() == 'PyPy':
+        import vector   # use pure python vector for PyPy
+        vector = vector.vector
+    else:
+        import cyvector.vector
+        vector = cyvector.vector
+except:
+    import cyvector
+    vector = cyvector.vector
+
+### Body class ###
+class Body(object):
+    __slots__ = ('mass', 'GM', 'velocity', 'position', 'temp_position', 'k', 'xv', 'radius', 'acc', 'name', 'index')
+    def __init__(self, mass=1, GM=1, radius=1, velocity=vector(0,0,0), position=vector(0,0,0), name="Body", index=0):
+        self.mass = mass
+        self.GM = GM
+        self.velocity = velocity
+        self.position = position
+        self.temp_position = vector(0,0,0)
+        self.k = []
+        self.xv = conVec(0,0)
+        self.radius = radius
+        self.acc = vector(0,0,0) #gravitational_acc(self.position, params) # approximate the initial acceleration for Verlet
+        self.name = name
+        self.index = index
+
+
+### Parameters class ###
+class parameters(object):
+    __slots__ = ('G', 'AU', 'M', 'Theta', 'Epsilon', 'Lambda', 'Chi', 'time', 'dt', 'scale', 'rate', 'config', 'useconfig', 'integrator', 'printEndPos', 'checkEndPos', 'args',
+                     'current_time', 'bodies', 'body_pairs', 'comets', 'all_bodies', 'start_time', 'end_time')
+    def __init__(self, args, G, AU, M, Theta, Epsilon, Lambda, Chi):
+
+        self.G = G
+        self.AU = AU
+        self.M = M
+        self.Theta = Theta
+        self.Epsilon = Epsilon
+        self.Lambda = Lambda
+        self.Chi = Chi
+
+        self.args = args
+        self.printEndPos = args.printEndPos
+        self.checkEndPos = args.checkEndPos
+
+        self.current_time = 0
+        self.bodies = []
+        self.comets = []
+        self.all_bodies = []
+        self.body_pairs = []
+
+        # load config from
+        with open(args.configfile, "r") as configfile:
+            config = json.load(configfile)
+        self.config = config
+        planet_config = config[0]
+        config = config[1] 
+        # load settings from configfile
+        if args.useconfig:
+            
+            if 'dt' in config:
+                self.dt = config['dt']
+            else:
+                self.dt = args.dt
+            
+            if 'time' in config:
+                self.end_time = config['time']
+            else:
+                self.end_time = args.time
+            
+            if 'integrator' in config:
+                self.integrator = config['integrator']
+            else:
+                self.integrator = args.integrator
+
+        else:
+            self.dt = args.dt
+            self.end_time = args.time
+            self.integrator = args.integrator
+        
+        # check which integrator was chosen
+        if self.integrator.lower() == "euler":
+            self.integrator = integrators.Euler
+        elif self.integrator.lower() == "rk4":
+            self.integrator = integrators.Runge_Kutta
+        elif self.integrator.lower() == "verlet":
+            self.integrator = integrators.Verlet
+        elif self.integrator.lower() == "fr":
+            self.integrator = integrators.Forest_Ruth
+        elif self.integrator.lower() == "pefrl":
+            self.integrator = integrators.PEFRL
+        else:
+            raise Exception('No valid integrator was provided')
+        
+        # load planets from configfile
+        for i, body in enumerate(planet_config):
+            # check to see if all needed fields are filled
+            if not 'mass' in body and not 'gm' in body:
+                raise Exception(f"Body nr. {i} don't have any mass properties")
+            else:
+                if 'gm' in body:
+                    gm = body['gm']
+                else:
+                    gm = body['mass'] * self.G
+                if 'mass' in body:
+                    mass = body['mass']
+                else:
+                    mass = body['gm']/self.G
+
+            if not 'radius' in body:
+                raise Exception(f"Body nr. {i} don't have any radius property")
+            else:
+                radius = body['radius']
+
+            if not 'position' in body:
+                raise Exception(f"Body nr. {i} don't have any position property")
+            else:
+                position = body['position']
+
+            if not 'velocity' in body:
+                raise Exception(f"Body nr. {i} don't have any velocity property")
+            else:
+                velocity = body['velocity']
+
+            if not 'name' in body:
+                print(f"WARNING: Body nr. {i} don't have a name property, default 'Body{i}'' will be used.")
+                name = f"Body{i}"
+            else:
+                name = body['name']
+
+            if not 'comet' in body:
+                print(f"WARNING: Body nr. {i} don't have a comet property, default 'false' will be used.")
+                comet = False
+            else:
+                comet = body['comet']
+            
+            # add planet to corresponding list
+            if comet:
+                self.comets.append(Body(
+                    name = name,
+                    mass=mass,
+                    GM=gm,
+                    radius=radius,
+                    position=vector(position[0], position[1], position[2]),
+                    velocity=vector(velocity[0], velocity[1], velocity[2]),
+                    index=len(self.bodies) + len(self.comets)
+                ))
+            else:
+                self.bodies.append(Body(
+                    name = name,
+                    mass=mass,
+                    GM=gm,
+                    radius=radius,
+                    position=vector(position[0], position[1], position[2]),
+                    velocity=vector(velocity[0], velocity[1], velocity[2]),
+                    index=len(self.bodies) + len(self.comets)
+                ))
+            
+            self.body_pairs = combinations(self.bodies, 2)
+            self.all_bodies = self.bodies + self.comets
+
+            # initialize acceleration for verlet
+            for body in self.all_bodies:
+                body.acc = integrators.gravitational_acc(body.position, self)
+            ### END parameters.__init__() ###
---
+++ integrators_headless.py
@@ -0,0 +1,156 @@
+from headless_init import conVec, vector
+
+### Integrators ###
+
+# Euler Integrator
+def Euler(p):
+    # acceleration and velocity calculations
+    for body in p.all_bodies:
+        body.acc = gravitational_acc(body.position, p)
+        body.velocity += p.dt * body.acc
+
+    # position calculations
+    for body in p.all_bodies:
+        body.position += p.dt * body.velocity
+
+# Runge-Kutta 4 (RK4) Integrator
+def Runge_Kutta(p):
+    
+    # calculate k1
+    for body in p.all_bodies:
+        body.k = [0] # zero at beginning to push indices one step up for readability
+        body.temp_position = body.position
+        body.xv = conVec(body.temp_position, body.velocity)
+        temp_k1 = gravitational_acc_runge(body.xv, p)
+        k1 = conVec(temp_k1.x * p.dt, temp_k1.y * p.dt)
+        body.k.append(k1)
+        
+    # move temp_pos according to k1
+    for body in p.all_bodies:
+        body.temp_position = body.position + body.k[1].x/2
+    
+    # calculate k2
+    for body in p.all_bodies:
+        temp_xv = conVec(body.xv.x + body.k[1].x/2, body.xv.y + body.k[1].y/2)
+        temp_k2 = gravitational_acc_runge(temp_xv, p)
+        k2 = conVec(temp_k2.x * p.dt, temp_k2.y * p.dt)
+        body.k.append(k2)
+    
+    # move temp_pos according to k2
+    for body in p.all_bodies:
+        body.temp_position = body.position + body.k[2].x/2
+
+    # calculate k3
+    for body in p.all_bodies:
+        temp_xv = conVec(body.xv.x + body.k[2].x/2, body.xv.y + body.k[2].y/2)
+        temp_k3 = gravitational_acc_runge(temp_xv, p)
+        k3 = conVec(temp_k3.x * p.dt, temp_k3.y * p.dt)
+        body.k.append(k3)
+    
+    # move temp_pos according to k3
+    for body in p.all_bodies:
+        body.temp_position = body.position + body.k[3].x
+
+    # calculate k4
+    for body in p.all_bodies:
+        temp_xv = conVec(body.xv.x + body.k[3].x, body.xv.y + body.k[3].y)
+        temp_k4 = gravitational_acc_runge(temp_xv, p)
+        k4 = conVec(temp_k4.x * p.dt, temp_k4.y * p.dt)
+        body.k.append(k4)
+
+    # calculate weighted sum and move bodies
+    for body in p.all_bodies:
+        body.position += 1/6 * (body.k[1].x + 2*body.k[2].x + 2*body.k[3].x + body.k[4].x)
+        body.velocity += 1/6 * (body.k[1].y + 2*body.k[2].y + 2*body.k[3].y + body.k[4].y)
+
+# Velocity-Verlet Integrator
+def Verlet(p):
+    # position calculations
+    for body in p.all_bodies:
+        body.position += body.velocity * p.dt + body.acc/2 * p.dt**2
+    # acceleration and velocity calculations
+    for body in p.all_bodies:
+        temp_acc = gravitational_acc(body.position, p)
+        body.velocity += p.dt/2*(body.acc + temp_acc)
+        body.acc = temp_acc
+
+def Forest_Ruth(p):
+    for body in p.all_bodies:
+        body.position += p.Theta*p.dt/2*body.velocity
+    for body in p.all_bodies:
+        body.velocity += p.Theta*p.dt*gravitational_acc(body.position, p)
+    for body in p.all_bodies:
+        body.position += (1-p.Theta)*p.dt/2*body.velocity
+    for body in p.all_bodies:
+        body.velocity += (1-2*p.Theta)*p.dt*gravitational_acc(body.position, p)
+    for body in p.all_bodies:
+        body.position += (1-p.Theta)*p.dt/2*body.velocity
+    for body in p.all_bodies:
+        body.velocity += p.Theta*p.dt*gravitational_acc(body.position, p)
+    for body in p.all_bodies:
+        body.position += p.Theta*p.dt/2*body.velocity
+
+def PEFRL(p):
+    for body in p.all_bodies:
+        body.position += p.Epsilon*p.dt*body.velocity
+    for body in p.all_bodies:
+        body.velocity += (1-2*p.Lambda)*p.dt/2*gravitational_acc(body.position, p)
+    for body in p.all_bodies:
+        body.position += p.Chi*p.dt*body.velocity
+    for body in p.all_bodies:
+        body.velocity += p.Lambda*p.dt*gravitational_acc(body.position, p)
+    for body in p.all_bodies:
+        body.position += (1-2*(p.Chi+p.Epsilon))*p.dt*body.velocity
+    for body in p.all_bodies:
+        body.velocity += p.Lambda*p.dt*gravitational_acc(body.position, p)
+    for body in p.all_bodies:
+        body.position += p.Chi*p.dt*body.velocity
+    for body in p.all_bodies:
+        body.velocity += (1-2*p.Lambda)*p.dt/2*gravitational_acc(body.position, p)
+    for body in p.all_bodies:
+        body.position += p.Epsilon*p.dt*body.velocity
+
+### End Integrators ###
+
+### Functions ###
+# gravitational acceleration for Euler and Verlet
+def gravitational_acc(position, p):
+    sum_acc = vector(0,0,0)
+    # calculate the gravitational acceleration from all other bodies
+    for body in p.bodies:
+        # distance to the other body
+        r_vec = body.position - position
+        r = r_vec.mag
+        # skip if body is itself
+        if r < body.radius:
+            continue
+        # the magnitude of the force
+        acc = body.GM / r_vec.mag2
+        # the unit vector for the force
+        dir = r_vec.hat
+        # the force vector
+        acc = acc * dir
+        # add force vector to the sum of forces
+        sum_acc += acc
+    return sum_acc
+
+# gravitational acceleration for Runge-Kutta
+def gravitational_acc_runge(xv, p):
+    sum_acc = vector(0,0,0)
+    # calculate the gravitational acceleration from all other bodies
+    for body in p.bodies:
+        # distance to the other body
+        r_vec = body.temp_position - xv.x
+        r = r_vec.mag
+        # skip if body is itself
+        if r < body.radius:
+            continue
+        # the magnitude of the force
+        acc = body.GM / r_vec.mag2
+        # the unit vector for the force
+        dir = r_vec.hat
+        # the force vector
+        acc = acc * dir
+        # add force vector to the sum of forces
+        sum_acc += acc
+    return conVec(xv.y, sum_acc)
